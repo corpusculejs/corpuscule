2fbe722e39a7012df87e9f0e8983eccefcc16dfb
Vlad Rindevich <rindevich.vs@gmail.com>
2018-04-24 19:03:52 +0300

    refactor(styles): create link inside an element

--- packages/styles/src/index.ts
+++ packages/styles/src/index.ts
@@ -1,16 +1,16 @@
 const Styles = (path: string) => (target: any) => {
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.type = 'text/css';
-  link.href = path;
-
-  const promise = new Promise((resolve) => {
-    link.onload = resolve as any;
-  });
-
   const {connectedCallback: superConnectedCallback} = target.prototype;
 
   target.prototype.connectedCallback = async function connectedCallback(this: any): Promise<void> {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = path;
+
+    const promise = new Promise((resolve) => {
+      link.onload = resolve as any;
+    });
+
     const layout = document.createElement('div');
     this.__root.appendChild(link);
     this.__root.appendChild(layout);
