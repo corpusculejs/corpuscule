f78926c5d334dfd72b082394a6412b695f86cbc6
Vlad Rindevich <rindevich.vs@gmail.com>
2018-05-08 12:40:08 +0300

    build(config): improve building process

---
+++ scripts/project.js
@@ -0,0 +1,11 @@
+const packages = {
+  element: [
+    'index',
+    'decorators',
+  ],
+  redux: ['index'],
+  router: ['index'],
+  styles: ['index'],
+};
+
+module.exports = packages;
---
+++ scripts/rollup.config.js
@@ -0,0 +1,31 @@
+const typescript = require('rollup-plugin-typescript2');
+const packages = require('./project');
+
+module.exports = Object.entries(packages).reduce((acc, [pack, entries]) => {
+  const ts = typescript({
+    clean: true,
+    tsconfig: 'tsconfig.json',
+    tsconfigOverride: {
+      compilerOptions: {
+        declaration: false,
+        types: [],
+      },
+      include: [
+        `packages/${pack}/src/**/*.ts`,
+      ],
+    }
+  });
+
+  for (const file of entries) {
+    acc.push({
+      input: `packages/${pack}/src/${file}.ts`,
+      output: {
+        file: `packages/${pack}/dist/${file}.js`,
+        format: 'es',
+      },
+      plugins: [ts],
+    });
+  }
+
+  return acc;
+}, []);
