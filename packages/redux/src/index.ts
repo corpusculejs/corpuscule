8c0773afd7c0f6ee8b495f6c2b073e9bd702ca99
Vlad Rindevich <rindevich.vs@gmail.com>
2018-05-10 16:51:11 +0300

    feat(redux): improve typings

--- packages/redux/src/index.ts
+++ packages/redux/src/index.ts
@@ -1,43 +1,45 @@
 // tslint:disable:no-invalid-this
 import {Action, Store} from 'redux';
-import {PropertyGetter} from './types';
+import {PropertyGetter, ReduxConstructor} from './types';
 import {initDispatchers, updateStoredProperties} from './utils';
 
-const redux = <S, A extends Action>(store: Store<S, A>) => <T = any>(target: any): T => {
-  const {
-    connectedCallback: superConnectedCallback,
-    disconnectedCallback: superDisconnectedCallback,
-  } = target.prototype;
-
-  if (target._dispatchers) {
-    initDispatchers(target, store, target._dispatchers);
-  }
-
-  if (target._stored) {
-    const registry: ReadonlyArray<[string, PropertyGetter<S>]> = Object.entries(target._stored);
-
-    target.prototype.connectedCallback = function connectedCallback(this: any): void {
-      updateStoredProperties(this, store, registry);
-
-      this.__unsubscribe = store.subscribe(() => {
-        updateStoredProperties(this, store, registry);
-      });
-
-      if (superConnectedCallback) {
-        superConnectedCallback.call(this);
+const withRedux =
+  <S, A extends Action>(store: Store<S, A>) =>
+    <T extends ReduxConstructor<S>>(target: T): T => {
+      if (target._dispatchers) {
+        initDispatchers(target, store, target._dispatchers);
       }
+
+      if (target._stored) {
+        const {
+          connectedCallback: superConnectedCallback,
+          disconnectedCallback: superDisconnectedCallback,
+        } = target.prototype;
+
+        const registry: ReadonlyArray<[string, PropertyGetter<S>]> = Object.entries(target._stored);
+
+        target.prototype.connectedCallback = function connectedCallback(this: any): void {
+          updateStoredProperties(this, store, registry);
+
+          this.__unsubscribe = store.subscribe(() => {
+            updateStoredProperties(this, store, registry);
+          });
+
+          if (superConnectedCallback) {
+            superConnectedCallback.call(this);
+          }
+        };
+
+        target.prototype.disconnectedCallback = function disconnectedCallback(this: any): void {
+          this.__unsubscribe();
+
+          if (superDisconnectedCallback) {
+            superDisconnectedCallback.call(this);
+          }
+        };
+      }
+
+      return target;
     };
 
-    target.prototype.disconnectedCallback = function disconnectedCallback(this: any): void {
-      this.__unsubscribe();
-
-      if (superDisconnectedCallback) {
-        superDisconnectedCallback.call(this);
-      }
-    };
-  }
-
-  return target;
-};
-
-export default redux;
+export default withRedux;
