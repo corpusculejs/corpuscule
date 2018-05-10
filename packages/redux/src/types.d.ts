5a776979f9d852182449e5cf4ffefcca899ba234
Vlad Rindevich <rindevich.vs@gmail.com>
2018-05-10 16:51:11 +0300

    feat(redux): improve typings

--- packages/redux/src/types.d.ts
+++ packages/redux/src/types.d.ts
@@ -1,2 +1,16 @@
 export type DispatcherRegistry = ReadonlyArray<string>;
 export type PropertyGetter<S> = (state: S) => any;
+
+// tslint:disable:no-method-signature
+export interface CustomElement extends HTMLElement {
+  attributeChangedCallback?(attrName: string, oldVal: string, newVal: string): void;
+  connectedCallback?(): void;
+  disconnectedCallback?(): void;
+  adoptedCallback?(): void;
+} // tslint:enable:no-method-signature
+
+export interface ReduxConstructor<S> {
+  readonly _dispatchers?: DispatcherRegistry;
+  readonly _stored?: {readonly [propertyName: string]: PropertyGetter<S>};
+  new (...args: any[]): CustomElement; // tslint:disable-line:readonly-array
+}
