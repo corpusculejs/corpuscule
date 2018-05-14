64ce703c0395c197708a7137167d92caabdf493d
Vlad Rindevich <rindevich.vs@gmail.com>
2018-05-14 14:22:23 +0300

    feat: use class inheritance instead of replacing prototypes

--- packages/redux/src/index.ts
+++ packages/redux/src/index.ts
@@ -1,5 +1,5 @@
 // tslint:disable:no-invalid-this
-import {Action, Store} from 'redux';
+import {Action, Store, Unsubscribe} from 'redux';
 import {PropertyGetter, ReduxConstructor} from './types';
 import {initDispatchers, updateStoredProperties} from './utils';
 
@@ -11,30 +11,29 @@ const withRedux =
       }
 
       if (target._stored) {
-        const {
-          connectedCallback: superConnectedCallback,
-          disconnectedCallback: superDisconnectedCallback,
-        } = target.prototype;
-
         const registry: ReadonlyArray<[string, PropertyGetter<S>]> = Object.entries(target._stored);
 
-        target.prototype.connectedCallback = function connectedCallback(this: any): void {
-          updateStoredProperties(this, store, registry);
+        return class WithRedux extends target {
+          private __unsubscribe?: Unsubscribe;
 
-          this.__unsubscribe = store.subscribe(() => {
+          public connectedCallback(): void {
             updateStoredProperties(this, store, registry);
-          });
 
-          if (superConnectedCallback) {
-            superConnectedCallback.call(this);
+            this.__unsubscribe = store.subscribe(() => {
+              updateStoredProperties(this, store, registry);
+            });
+
+            if (super.connectedCallback) {
+              super.connectedCallback();
+            }
           }
-        };
 
-        target.prototype.disconnectedCallback = function disconnectedCallback(this: any): void {
-          this.__unsubscribe();
+          public disconnectedCallback(): void {
+            this.__unsubscribe!();
 
-          if (superDisconnectedCallback) {
-            superDisconnectedCallback.call(this);
+            if (super.disconnectedCallback) {
+              super.disconnectedCallback();
+            }
           }
         };
       }
--- packages/styles/src/index.ts
+++ packages/styles/src/index.ts
@@ -4,34 +4,45 @@ import {StyleConstructor} from './types';
 const stylePattern = /[{}]/;
 
 const styles = (pathOrStyle: string) => <T extends StyleConstructor>(target: T): T => {
-  const {
-    _createRoot: superCreateRoot,
-    connectedCallback: superConnectedCallback,
-  } = target.prototype;
-
   if (stylePattern.test(pathOrStyle)) {
-    target.prototype._createRoot = function _createRoot(this: any): HTMLDivElement {
-      const root = superCreateRoot.call(this);
+    return class WithStyle extends target {
+      protected _createRoot(): HTMLDivElement {
+        // @ts-ignore
+        const root = super._createRoot();
 
-      const style = document.createElement('style');
-      style.textContent = pathOrStyle;
+        const style = document.createElement('style');
+        style.textContent = pathOrStyle;
 
-      const layout = document.createElement('div');
-      root.appendChild(style);
-      root.appendChild(layout);
+        const layout = document.createElement('div');
+        root.appendChild(style);
+        root.appendChild(layout);
 
-      return layout;
+        return layout;
+      }
     };
-  } else {
-    target.prototype._createRoot = function _createRoot(this: any): HTMLDivElement {
-      const root = superCreateRoot.call(this);
+  }
+
+  return class WithLink extends target {
+    private __loading?: Promise<void>;
+
+    public async connectedCallback(): Promise<void> {
+      await this.__loading;
+
+      if (super.connectedCallback) {
+        super.connectedCallback();
+      }
+    }
+
+    protected _createRoot(): HTMLDivElement {
+      // @ts-ignore
+      const root = super._createRoot();
 
       const link = document.createElement('link');
       link.rel = 'stylesheet';
       link.type = 'text/css';
       link.href = pathOrStyle;
 
-      this.__linkLoadingPromise = new Promise<void>((resolve) => {
+      this.__loading = new Promise<void>((resolve) => {
         link.onload = resolve as any;
       });
 
@@ -40,18 +51,8 @@ const styles = (pathOrStyle: string) => <T extends StyleConstructor>(target: T):
       root.appendChild(layout);
 
       return layout;
-    };
-
-    target.prototype.connectedCallback = async function connectedCallback(this: any): Promise<void> {
-      await this.__linkLoadingPromise;
-
-      if (superConnectedCallback) {
-        superConnectedCallback.call(this);
-      }
-    };
-  }
-
-  return target;
+    }
+  };
 };
 
 export default styles;
