ce6443a1a5aae7677a41573c3ff934f90c1245c6
Vlad Rindevich <rindevich.vs@gmail.com>
2018-05-15 17:46:25 +0300

    refactor(redux): make connection functions static

--- packages/redux/src/index.ts
+++ packages/redux/src/index.ts
@@ -3,42 +3,60 @@ import {Action, Store, Unsubscribe} from 'redux';
 import {PropertyGetter, ReduxConstructor} from './types';
 import {initDispatchers, updateStoredProperties} from './utils';
 
-const withRedux =
-  <S, A extends Action>(store: Store<S, A>) =>
-    <T extends ReduxConstructor<S>>(target: T): T => {
-      if (target._dispatchers) {
-        initDispatchers(target, store, target._dispatchers);
-      }
+const createReduxConnectionUtils = () => {
+  let currentStore: Store<any, any>;
 
-      if (target._stored) {
-        const registry: ReadonlyArray<[string, PropertyGetter<S>]> = Object.entries(target._stored);
+  const provideReduxStore = <S, A extends Action>(store: Store<S, A>): void => {
+    currentStore = store;
+  };
 
-        return class WithRedux extends target {
-          private __unsubscribe?: Unsubscribe;
+  const connectReduxStore = <S, T extends ReduxConstructor<S>>(target: T): T => {
+    if (!currentStore) {
+      throw new Error('Store is not provided');
+    }
 
-          public connectedCallback(): void {
-            updateStoredProperties(this, store, registry);
+    if (target._dispatchers) {
+      initDispatchers(target, currentStore, target._dispatchers);
+    }
 
-            this.__unsubscribe = store.subscribe(() => {
-              updateStoredProperties(this, store, registry);
-            });
+    if (target._stored) {
+      const registry: ReadonlyArray<[string, PropertyGetter<S>]> = Object.entries(target._stored);
 
-            if (super.connectedCallback) {
-              super.connectedCallback();
-            }
+      return class WithRedux extends target {
+        private __unsubscribe?: Unsubscribe;
+
+        public connectedCallback(): void {
+          updateStoredProperties(this, currentStore, registry);
+
+          this.__unsubscribe = currentStore.subscribe(() => {
+            updateStoredProperties(this, currentStore, registry);
+          });
+
+          if (super.connectedCallback) {
+            super.connectedCallback();
           }
+        }
 
-          public disconnectedCallback(): void {
-            this.__unsubscribe!();
+        public disconnectedCallback(): void {
+          this.__unsubscribe!();
 
-            if (super.disconnectedCallback) {
-              super.disconnectedCallback();
-            }
+          if (super.disconnectedCallback) {
+            super.disconnectedCallback();
           }
-        };
-      }
+        }
+      };
+    }
 
-      return target;
-    };
+    return target;
+  };
 
-export default withRedux;
+  return {
+    connect: connectReduxStore,
+    provide: provideReduxStore,
+  };
+};
+
+export const {
+  connect,
+  provide,
+} = createReduxConnectionUtils();
