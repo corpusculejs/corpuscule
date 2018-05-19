29a65088b025376e8575b3919d33163561c7d056
Vlad Rindevich <rindevich.vs@gmail.com>
2018-05-20 02:01:07 +0300

    test(element): add tests for decorators

---
+++ packages/element/__tests__/decorators.ts
@@ -0,0 +1,144 @@
+// tslint:disable:await-promise max-classes-per-file
+import {TemplateResult} from 'lit-html';
+import {html} from 'lit-html/lib/lit-extended';
+// tslint:disable-next-line:no-implicit-dependencies
+import uuid from 'uuid/v4';
+import CorpusculeElement from '../src';
+import {attribute, computed, element, property, state} from '../src/decorators';
+import {mount} from './utils';
+
+describe('Decorators', () => {
+  describe('@element', () => {
+    it('should init CorpusculeElement', () => {
+      const is = `x-${uuid()}`;
+
+      @element(is)
+      class Test extends CorpusculeElement {
+        protected _render(): TemplateResult {
+          return html`<span id="node">Test content</span>`;
+        }
+      }
+
+      const el = mount(is);
+      expect(el).toEqual(jasmine.any(Test));
+
+      const root = el.shadowRoot;
+      expect(root).not.toBeNull();
+
+      const node = root!.getElementById('node');
+      expect(node).not.toBeNull();
+      expect(node!.textContent).toBe('Test content');
+    });
+
+    it('should add static "is" to element', () => {
+      const is = `x-${uuid()}`;
+
+      @element(is)
+      class Test extends CorpusculeElement {
+        protected _render(): TemplateResult {
+          return html`<span id="node">Test content</span>`;
+        }
+      }
+
+      expect(Test.is).toBe(is);
+    });
+  });
+
+  describe('@attribute', () => {
+    it('should add attribute to element', () => {
+      class Test extends CorpusculeElement {
+        @attribute('t', Number)
+        public test: number = 1;
+
+        @attribute('t2', Boolean, {pure: false})
+        public test2: boolean = false;
+
+        protected _render(): TemplateResult {
+          return html`<span id="node">Test content</span>`;
+        }
+      }
+
+      expect((Test as any)._attributes).toEqual({
+        test: ['t', Number],
+        test2: ['t2', Boolean, {pure: false}],
+      });
+    });
+  });
+
+  describe('@property', () => {
+    it('should add property to element', () => {
+      const guard1 = (v: any) => typeof v === 'boolean';
+      const guard2 = (v: any) => typeof v === 'number';
+
+      class Test extends CorpusculeElement {
+        @property()
+        public test: number = 1;
+
+        @property(guard1)
+        public test2: boolean = false;
+
+        @property(guard2, {pure: false})
+        public test3: number = 2;
+
+        protected _render(): TemplateResult {
+          return html`<span id="node">Test content</span>`;
+        }
+      }
+
+      expect((Test as any)._properties).toEqual({
+        test: null,
+        test2: guard1,
+        test3: [guard2, {pure: false}],
+      });
+    });
+  });
+
+  describe('@state', () => {
+    it('should add state property to element', () => {
+      class Test extends CorpusculeElement {
+        @state
+        public test: number = 1;
+
+        @state
+        public test2: number = 2;
+
+        protected _render(): TemplateResult {
+          return html`<span id="node">Test content</span>`;
+        }
+      }
+
+      expect((Test as any)._states).toEqual([
+        'test',
+        'test2',
+      ]);
+    });
+  });
+
+  describe('@computed', () => {
+    it('should add computed property to element', () => {
+      class Test extends CorpusculeElement {
+        public first: number = 1;
+        public second: number = 2;
+
+        @computed('first', 'second')
+        public get test(): number {
+          return this.first + this.second;
+        }
+
+        @computed('first', 'second')
+        public get test2(): number {
+          return this.second - this.first;
+        }
+
+        protected _render(): TemplateResult {
+          return html`<span id="node">Test content</span>`;
+        }
+      }
+
+      expect((Test as any)._computed).toEqual({
+        test: ['first', 'second'],
+        test2: ['first', 'second'],
+      });
+    });
+  });
+});
