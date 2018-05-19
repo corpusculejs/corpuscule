1786fee06c79d5059c5e1b19e6872de737f01ab3
Vlad Rindevich <rindevich.vs@gmail.com>
2018-05-19 21:30:11 +0300

    refactor(styles): add ability to set multiple files or styles in single decorator

--- packages/styles/src/index.ts
+++ packages/styles/src/index.ts
@@ -3,30 +3,15 @@ import {StyleConstructor} from './types';
 
 const stylePattern = /[{}]/;
 
-const styles = (pathOrStyle: string) => <T extends StyleConstructor>(target: T): T => {
-  if (stylePattern.test(pathOrStyle)) {
-    return class WithStyle extends target {
-      protected _createRoot(): HTMLDivElement {
-        // @ts-ignore
-        const root = super._createRoot();
-
-        const style = document.createElement('style');
-        style.textContent = pathOrStyle;
-
-        const layout = document.createElement('div');
-        root.appendChild(style);
-        root.appendChild(layout);
-
-        return layout;
-      }
-    };
-  }
-
-  return class WithLink extends target {
-    private __loading?: Promise<void>;
+// tslint:disable-next-line:readonly-array
+const styles = (...pathsOrStyles: string[]) => <T extends StyleConstructor>(target: T): T =>
+  class WithStyles extends target {
+    private __loading?: Promise<void[]>;
 
     public async connectedCallback(): Promise<void> {
-      await this.__loading;
+      if (this.__loading) {
+        await this.__loading;
+      }
 
       if (super.connectedCallback) {
         super.connectedCallback();
@@ -36,23 +21,36 @@ const styles = (pathOrStyle: string) => <T extends StyleConstructor>(target: T):
     protected _createRoot(): HTMLDivElement {
       // @ts-ignore
       const root = super._createRoot();
-
-      const link = document.createElement('link');
-      link.rel = 'stylesheet';
-      link.type = 'text/css';
-      link.href = pathOrStyle;
-
-      this.__loading = new Promise<void>((resolve) => {
-        link.onload = resolve as any;
-      });
-
       const layout = document.createElement('div');
-      root.appendChild(link);
+      const loadingPromises: Array<Promise<void>> = [];
+
+      for (const pathOrStyle of pathsOrStyles) {
+        if (stylePattern.test(pathOrStyle)) {
+          const style = document.createElement('style');
+          style.textContent = pathOrStyle;
+          root.appendChild(style);
+        } else {
+          const link = document.createElement('link');
+          link.rel = 'stylesheet';
+          link.type = 'text/css';
+          link.href = pathOrStyle;
+
+          loadingPromises.push(new Promise<void>((resolve) => {
+            link.onload = resolve as any;
+          }));
+
+          root.appendChild(link);
+        }
+      }
+
+      if (loadingPromises.length > 0) {
+        this.__loading = Promise.all(loadingPromises);
+      }
+
       root.appendChild(layout);
 
       return layout;
     }
   };
-};
 
 export default styles;
