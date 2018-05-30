668abb6faf9e009e914120587ce8cb8cf268796f
Vlad Rindevich <rindevich.vs@gmail.com>
2018-05-30 21:05:39 +0300

    test(element): make single test root calling "@corpuscule/element"

--- packages/element/__tests__/decorators.ts
+++ packages/element/__tests__/decorators.ts
@@ -7,138 +7,142 @@ import CorpusculeElement, {attributeMap, computedMap, propertyMap, render, state
 import {attribute, computed, element, property, state} from '../src/decorators';
 import {mount} from './utils';
 
-describe('Decorators', () => {
-  describe('@element', () => {
-    it('should init CorpusculeElement', () => {
-      const is = `x-${uuid()}`;
+const decorators = () => {
+  describe('decorators', () => {
+    describe('@element', () => {
+      it('should init CorpusculeElement', () => {
+        const is = `x-${uuid()}`;
 
-      @element(is)
-      class Test extends CorpusculeElement {
-        protected [render](): TemplateResult {
-          return html`<span id="node">Test content</span>`;
+        @element(is)
+        class Test extends CorpusculeElement {
+          protected [render](): TemplateResult {
+            return html`<span id="node">Test content</span>`;
+          }
         }
-      }
 
-      const el = mount(is);
-      expect(el).toEqual(jasmine.any(Test));
+        const el = mount(is);
+        expect(el).toEqual(jasmine.any(Test));
 
-      const root = el.shadowRoot;
-      expect(root).not.toBeNull();
+        const root = el.shadowRoot;
+        expect(root).not.toBeNull();
 
-      const node = root!.getElementById('node');
-      expect(node).not.toBeNull();
-      expect(node!.textContent).toBe('Test content');
+        const node = root!.getElementById('node');
+        expect(node).not.toBeNull();
+        expect(node!.textContent).toBe('Test content');
+      });
+
+      it('should add static "is" to element', () => {
+        const is = `x-${uuid()}`;
+
+        @element(is)
+        class Test extends CorpusculeElement {
+          protected [render](): TemplateResult {
+            return html`<span id="node">Test content</span>`;
+          }
+        }
+
+        expect(Test.is).toBe(is);
+      });
     });
 
-    it('should add static "is" to element', () => {
-      const is = `x-${uuid()}`;
+    describe('@attribute', () => {
+      it('should add attribute to element', () => {
+        class Test extends CorpusculeElement {
+          @attribute('t', Number)
+          public test: number = 1;
 
-      @element(is)
-      class Test extends CorpusculeElement {
-        protected [render](): TemplateResult {
-          return html`<span id="node">Test content</span>`;
+          @attribute('t2', Boolean, {pure: false})
+          public test2: boolean = false;
+
+          protected [render](): TemplateResult {
+            return html`<span id="node">Test content</span>`;
+          }
         }
-      }
 
-      expect(Test.is).toBe(is);
+        expect((Test as any)[attributeMap]).toEqual({
+          test: ['t', Number],
+          test2: ['t2', Boolean, {pure: false}],
+        });
+      });
     });
-  });
 
-  describe('@attribute', () => {
-    it('should add attribute to element', () => {
-      class Test extends CorpusculeElement {
-        @attribute('t', Number)
-        public test: number = 1;
+    describe('@property', () => {
+      it('should add property to element', () => {
+        const guard1 = (v: any) => typeof v === 'boolean';
+        const guard2 = (v: any) => typeof v === 'number';
 
-        @attribute('t2', Boolean, {pure: false})
-        public test2: boolean = false;
+        class Test extends CorpusculeElement {
+          @property()
+          public test: number = 1;
 
-        protected [render](): TemplateResult {
-          return html`<span id="node">Test content</span>`;
+          @property(guard1)
+          public test2: boolean = false;
+
+          @property(guard2, {pure: false})
+          public test3: number = 2;
+
+          protected [render](): TemplateResult {
+            return html`<span id="node">Test content</span>`;
+          }
         }
-      }
 
-      expect((Test as any)[attributeMap]).toEqual({
-        test: ['t', Number],
-        test2: ['t2', Boolean, {pure: false}],
+        expect((Test as any)[propertyMap]).toEqual({
+          test: null,
+          test2: guard1,
+          test3: [guard2, {pure: false}],
+        });
+      });
+    });
+
+    describe('@state', () => {
+      it('should add state property to element', () => {
+        class Test extends CorpusculeElement {
+          @state
+          public test: number = 1;
+
+          @state
+          public test2: number = 2;
+
+          protected [render](): TemplateResult {
+            return html`<span id="node">Test content</span>`;
+          }
+        }
+
+        expect((Test as any)[stateMap]).toEqual([
+          'test',
+          'test2',
+        ]);
+      });
+    });
+
+    describe('@computed', () => {
+      it('should add computed property to element', () => {
+        class Test extends CorpusculeElement {
+          public first: number = 1;
+          public second: number = 2;
+
+          @computed('first', 'second')
+          public get test(): number {
+            return this.first + this.second;
+          }
+
+          @computed('first', 'second')
+          public get test2(): number {
+            return this.second - this.first;
+          }
+
+          protected [render](): TemplateResult {
+            return html`<span id="node">Test content</span>`;
+          }
+        }
+
+        expect((Test as any)[computedMap]).toEqual({
+          test: ['first', 'second'],
+          test2: ['first', 'second'],
+        });
       });
     });
   });
+};
 
-  describe('@property', () => {
-    it('should add property to element', () => {
-      const guard1 = (v: any) => typeof v === 'boolean';
-      const guard2 = (v: any) => typeof v === 'number';
-
-      class Test extends CorpusculeElement {
-        @property()
-        public test: number = 1;
-
-        @property(guard1)
-        public test2: boolean = false;
-
-        @property(guard2, {pure: false})
-        public test3: number = 2;
-
-        protected [render](): TemplateResult {
-          return html`<span id="node">Test content</span>`;
-        }
-      }
-
-      expect((Test as any)[propertyMap]).toEqual({
-        test: null,
-        test2: guard1,
-        test3: [guard2, {pure: false}],
-      });
-    });
-  });
-
-  describe('@state', () => {
-    it('should add state property to element', () => {
-      class Test extends CorpusculeElement {
-        @state
-        public test: number = 1;
-
-        @state
-        public test2: number = 2;
-
-        protected [render](): TemplateResult {
-          return html`<span id="node">Test content</span>`;
-        }
-      }
-
-      expect((Test as any)[stateMap]).toEqual([
-        'test',
-        'test2',
-      ]);
-    });
-  });
-
-  describe('@computed', () => {
-    it('should add computed property to element', () => {
-      class Test extends CorpusculeElement {
-        public first: number = 1;
-        public second: number = 2;
-
-        @computed('first', 'second')
-        public get test(): number {
-          return this.first + this.second;
-        }
-
-        @computed('first', 'second')
-        public get test2(): number {
-          return this.second - this.first;
-        }
-
-        protected [render](): TemplateResult {
-          return html`<span id="node">Test content</span>`;
-        }
-      }
-
-      expect((Test as any)[computedMap]).toEqual({
-        test: ['first', 'second'],
-        test2: ['first', 'second'],
-      });
-    });
-  });
-});
+export default decorators;
