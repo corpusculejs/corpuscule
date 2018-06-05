4731313b8d71a0d99a1dfba73dc1061eadc94f93
Vlad Rindevich <rindevich.vs@gmail.com>
2018-06-05 20:16:56 +0300

    refactor(redux): rewrite package in esnext with d.ts using @corpuscule/context

--- packages/redux/src/tokens.ts
+++
@@ -1,4 +0,0 @@
-export const storedMap = Symbol("storedMap");
-export const dispatcherMap = Symbol("dispatcherMap");
-
-export const unsubscribe = Symbol();
---
+++ packages/redux/src/tokens/lifecycle.js
@@ -0,0 +1,1 @@
+export const storedMap = Symbol("storedMap");
