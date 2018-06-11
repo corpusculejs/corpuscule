08228b0283d925591660a3482f52a8743a205ae6
Vlad Rindevich <rindevich.vs@gmail.com>
2018-06-12 01:02:42 +0300

    refactor(redux): critical improvements
    * Allow to use decorators
    * Allow to collect connected properties from whole prototype chain

--- packages/redux/src/tokens/internal.js
+++ packages/redux/src/tokens/internal.js
@@ -1,3 +1,6 @@
+export const context = Symbol("context");
+export const initDispatchers = Symbol("initDispatchers");
+export const memoizedRegistry = Symbol("memoizedRegistry");
 export const registry = Symbol("registry");
 export const subscribe = Symbol("subscribe");
 export const unsubscribe = Symbol("unsubscribe");
