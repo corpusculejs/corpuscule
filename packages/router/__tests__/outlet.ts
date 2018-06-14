8c3045e37b26ed0831b30a6c90dc9513488efd95
Vlad Rindevich <rindevich.vs@gmail.com>
2018-06-15 00:52:27 +0300

    test(router): improve tests & add test for outlet

---
+++ packages/router/__tests__/outlet.ts
@@ -0,0 +1,70 @@
+// tslint:disable:max-classes-per-file
+import UniversalRouter, {Route} from "universal-router";
+// tslint:disable-next-line:no-implicit-dependencies
+import uuid from "uuid/v4";
+import {BasicConsumer, BasicProvider, defineAndMountContext} from "../../../test/utils";
+import {createRouter, layout, outlet, provider, router as $router} from "../src";
+
+const outletTest = () => {
+  describe("outlet", () => {
+    const routes: ReadonlyArray<Route> = [
+      {
+        action: () => "Test Root",
+        path: "",
+      },
+      {
+        action: () => "Test Branch",
+        path: `#test`,
+      },
+    ];
+
+    const router = createRouter(routes, {
+      baseUrl: location.pathname,
+    });
+
+    it("should create a router outlet that contains initial layout", async () => {
+      @provider
+      class Provider extends BasicProvider {
+        public static is: string = `x-${uuid()}`;
+
+        protected readonly [$router]: UniversalRouter = router;
+      }
+
+      class Test extends outlet(routes)(BasicConsumer) {
+        public static is: string = `x-${uuid()}`;
+
+        public readonly [layout]: string;
+      }
+
+      const [, o] = defineAndMountContext(Provider, Test);
+
+      await o.resolvingPromise;
+
+      expect(o[layout]).toBe("Test Root");
+    });
+
+    it("should react to a 'popstate' event and ", async () => {
+      class Provider extends provider(BasicProvider) {
+        public static is: string = `x-${uuid()}`;
+
+        protected readonly [$router]: UniversalRouter = router;
+      }
+
+      class Test extends outlet(routes)(BasicConsumer) {
+        public static is: string = `x-${uuid()}`;
+
+        public readonly [layout]: string;
+      }
+
+      const [, o] = defineAndMountContext(Provider, Test);
+
+      dispatchEvent(new PopStateEvent("popstate", {state: "#test"}));
+
+      await o.resolvingPromise;
+
+      expect(o[layout]).toBe("Test Branch");
+    });
+  });
+};
+
+export default outletTest;
