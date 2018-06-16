7722e0d78cbac31f237b7fea54bcf12b19c91b31
Vlad Rindevich <rindevich.vs@gmail.com>
2018-06-16 23:47:30 +0700

    refactor(redux): remove decorators

--- packages/redux/src/decorators.d.ts
+++
@@ -1,7 +0,0 @@
-import {PropertyGetter} from ".";
-
-export const connected:
-  <S = any>(getter: PropertyGetter<S>) =>
-    (target: any, propertyKey: PropertyKey) => void;
-
-export const dispatcher: (target: any, propertyKey: PropertyKey) => void;
