047795e4cf0c48383ebf4bd1fe3c3c1e36dd7531
Vlad Rindevich <rindevich.vs@gmail.com>
2018-06-16 23:54:41 +0700

    refactor(styles): drop ts-decorators support

--- packages/styles/src/index.d.ts
+++ packages/styles/src/index.d.ts
@@ -1,16 +1,16 @@
-import {UncertainCustomElementClass} from "@corpuscule/types";
+import {CustomElementClass, UncertainCustomElementClass} from "@corpuscule/types";
 import {TemplateResult} from "lit-html";
 
 export const link: (url: string, base: string) => string;
 
 export const style: unique symbol;
 
-export interface StylesStatic {
+export interface StylesClass<T> extends CustomElementClass<T> {
   readonly [style]: TemplateResult;
 }
 
 declare const styles:
   (...pathsOrStyles: string[]) => // tslint:disable-line:readonly-array
-    <T extends UncertainCustomElementClass<T>>(target: T) => T & StylesStatic;
+    <T>(target: UncertainCustomElementClass<T>) => StylesClass<T>;
 
 export default styles;
