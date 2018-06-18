08f3da469e945473f48c8f9fa358595e57f8311a
Vlad Rindevich <rindevich.vs@gmail.com>
2018-06-18 17:26:29 +0700

    fix(redux): initialize redux-connected component in "observedAttributes" getter

--- packages/redux/src/tokens/internal.js
+++ packages/redux/src/tokens/internal.js
@@ -1,6 +1,5 @@
 export const context = Symbol("context");
 export const initDispatchers = Symbol("initDispatchers");
-export const memoizedRegistry = Symbol("memoizedRegistry");
 export const registry = Symbol("registry");
 export const subscribe = Symbol("subscribe");
 export const unsubscribe = Symbol("unsubscribe");
