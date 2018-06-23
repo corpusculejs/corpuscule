9a4ca76205f51913ada959e87a69520adb3268af
Vlad Rindevich <rindevich.vs@gmail.com>
2018-06-24 01:47:03 +0700

    refactor(types): update Omit type to a new typescript version

--- packages/types/src/index.d.ts
+++ packages/types/src/index.d.ts
@@ -1,5 +1,5 @@
 // tslint:disable-next-line:max-line-length readonly-keyword
-export type Omit<T, K extends keyof T> = Pick<T, ({[P in keyof T]: P} & {[P in K]: never} & {[x: string]: never, [x: number]: never})[keyof T]>;
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 export interface CustomElement extends HTMLElement {
   attributeChangedCallback?(attrName: string, oldVal: string, newVal: string): void;
