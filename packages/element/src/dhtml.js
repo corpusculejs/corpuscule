1a97cb8329e5942898d3268799536db5293cf0bf
Vlad Rindevich <rindevich.vs@gmail.com>
2018-10-09 08:05:33 +0300

    refactor(element): simplify dhtml code

--- packages/element/src/dhtml.js
+++ packages/element/src/dhtml.js
@@ -11,48 +11,42 @@ export const unsafeStatic = value => new UnsafeStatic(value);
 
 const registry = new WeakMap();
 
-const dhtml = (strings, ...values) => {
-  const record = registry.get(strings);
+const isUnsafeStatic = value => value instanceof UnsafeStatic;
 
-  let recordStrings;
-  let valueIndexesToFilter;
+const dhtml = (strings, ...values) => {
+  let record = registry.get(strings);
 
   if (!record) {
-    valueIndexesToFilter = [];
-
-    if (values.some(value => value instanceof UnsafeStatic)) {
-      recordStrings = [];
+    if (values.some(isUnsafeStatic)) {
+      record = [];
 
       let previousValueWasStatic = false;
 
       for (let i = 0; i < strings.length; i++) {
         if (previousValueWasStatic) {
-          recordStrings[recordStrings.length - 1] += strings[i];
+          record[record.length - 1] += strings[i];
         } else {
-          recordStrings.push(strings[i]);
+          record.push(strings[i]);
         }
 
-        if (i < strings.length - 1 && values[i] instanceof UnsafeStatic) {
-          recordStrings[recordStrings.length - 1] += String(values[i].value);
-          valueIndexesToFilter.push(i);
+        if (i < strings.length - 1 && isUnsafeStatic(values[i])) {
+          record[record.length - 1] += String(values[i].value);
           previousValueWasStatic = true;
         } else {
           previousValueWasStatic = false;
         }
       }
     } else {
-      recordStrings = strings;
+      record = strings;
     }
 
-    registry.set(strings, [recordStrings, valueIndexesToFilter]);
-  } else {
-    [recordStrings, valueIndexesToFilter] = record;
+    registry.set(strings, record);
   }
 
   return html(
-    recordStrings,
-    ...valueIndexesToFilter.length > 0
-      ? values.filter((_, i) => !valueIndexesToFilter.includes(i))
+    record,
+    ...record !== strings
+      ? values.filter(value => !isUnsafeStatic(value))
       : values,
   );
 };
