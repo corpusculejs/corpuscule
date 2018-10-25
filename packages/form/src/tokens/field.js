c6a6f15e87f3954dd40463430381cbf7477f38c7
Vlad Rindevich <rindevich.vs@gmail.com>
2018-10-25 14:57:53 +0300

    refactor(form): use symbols for final-form properties

---
+++ packages/form/src/tokens/field.js
@@ -0,0 +1,9 @@
+export const allowNull = Symbol("allowNull");
+export const format = Symbol("format");
+export const formatOnBlur = Symbol("formatOnBlur");
+export const isEqual = Symbol("isEqual");
+export const name = Symbol("name");
+export const parse = Symbol("parse");
+export const subscription = Symbol("subscription");
+export const validate = Symbol("validate");
+export const value = Symbol("value");
