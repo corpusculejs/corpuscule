68e31d63fd9a4e8fd15369ad3ba8ebceda014f0a
Vlad Rindevich <rindevich.vs@gmail.com>
2018-10-25 16:53:14 +0300

    refactor(form): improve code

--- packages/form/src/index.js
+++ packages/form/src/index.js
@@ -1,4 +1,5 @@
 export {providingValue as finalForm} from "./context";
 // Export {default as field} from "./field";
 export {default as form} from "./form";
-export * from "./tokens/lifecycle";
+export * from "./tokens/field";
+export * from "./tokens/form";
--- packages/form/src/tokens/form.js
+++ packages/form/src/tokens/form.js
@@ -4,7 +4,10 @@ export const initialValues = Symbol("initialValues");
 export const keepDirtyOnReinitialize = Symbol("keepDirtyOnReinitialize");
 export const mutators = Symbol("mutators");
 export const onSubmit = Symbol("onSubmit");
-export const validate = Symbol("validate");
+export const validateForm = Symbol("validateForm");
 export const validateOnBlur = Symbol("validateOnBlur");
 
+export const formApi = Symbol("formApi");
+export const formState = Symbol("formState");
 export const initialValuesEqual = Symbol("initialValuesEqual");
+
