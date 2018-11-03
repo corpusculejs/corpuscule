9ee65b27f4a2b673addf595d498df44d5a7d65ce
Vlad Rindevich <rindevich.vs@gmail.com>
2018-11-04 00:25:09 +0300

    refactor(element): add new implementation of state decorator

---
+++ packages/element/src/state.js
@@ -0,0 +1,44 @@
+import {assertElementDecoratorsKindAndPlacement} from './utils';
+import {invalidate as $$invalidate} from './tokens/internal';
+import {stateChangedCallback as $stateChangedCallback} from './tokens/lifecycle';
+
+const state = ({
+  initializer,
+  key,
+  kind,
+  placement,
+}) => {
+  assertElementDecoratorsKindAndPlacement('state', kind, placement);
+
+  const storage = Symbol();
+
+  return {
+    descriptor: {
+      configurable: true,
+      enumerable: true,
+      get() {
+        return this[storage];
+      },
+      set(value) {
+        this[$stateChangedCallback](key, this[storage], value);
+
+        this[storage] = value;
+        this[$$invalidate]();
+      },
+    },
+    extras: [
+      {
+        descriptor: {},
+        initializer,
+        key: storage,
+        kind: 'field',
+        placement: 'own',
+      },
+    ],
+    key,
+    kind: 'method',
+    placement: 'prototype',
+  };
+};
+
+export default state;
