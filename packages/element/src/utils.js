4ba3283b53b4ff6b6f55dba0dc4cbf7f3d1ac200
Vlad Rindevich <rindevich.vs@gmail.com>
2018-11-03 04:03:33 +0300

    refactor(element): add new implementation of attribute decorator

--- packages/element/src/utils.js
+++ packages/element/src/utils.js
@@ -22,3 +22,20 @@ export const toAttribute = (instance, attributeName, value) => {
     instance.setAttribute(attributeName, value.toString());
   }
 };
+
+export const assertElementDecoratorsKindAndPlacement = (decoratorName, kind, placement) => {
+  if (kind !== 'field') {
+    throw new TypeError(
+      `@${decoratorName} can be applied only to field, not to ${kind}. `
+      + `Also @${decoratorName} expected to be the first executed decorator, so pay attention `
+      + 'to an order of your decorators',
+    );
+  }
+
+  if (placement !== 'own') {
+    throw new TypeError(
+      `@${decoratorName} can only be applied to an instance field, `
+      + `it is unusable with ${placement}`,
+    );
+  }
+};
