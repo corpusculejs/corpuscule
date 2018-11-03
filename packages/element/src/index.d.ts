c7c31999592b0f6718455891efa9721b49b7e5b3
Vlad Rindevich <rindevich.vs@gmail.com>
2018-11-04 00:39:16 +0300

    refactor(element): add new typings

--- packages/element/src/index.d.ts
+++ packages/element/src/index.d.ts
@@ -2,28 +2,31 @@
 import {CustomElement} from '@corpuscule/typings';
 import {TemplateResult} from 'lit-html';
 
-export interface PropertyOptions {
-  readonly pure?: boolean;
+export interface ComputingPair {
+  readonly computed: MethodDecorator;
+  readonly observer: PropertyDecorator;
 }
 
 export type AttributeGuard = BooleanConstructor | NumberConstructor | StringConstructor;
 export type PropertyGuard = (value: unknown) => boolean;
 
-export const attribute: (attributeName: string, guard: AttributeGuard, options?: PropertyOptions) => PropertyDecorator;
+export const attribute: (attributeName: string, guard: AttributeGuard) => PropertyDecorator;
 export const element: (name: string) => ClassDecorator;
-export const property: (guard?: PropertyGuard, options?: PropertyOptions) => PropertyDecorator;
+export const property: (guard?: PropertyGuard) => PropertyDecorator;
 export const state: PropertyDecorator;
 
+export const createComputingPair: () => ComputingPair;
+
 export const createRoot: unique symbol;
-export const didMount: unique symbol;
 export const didUpdate: unique symbol;
-export const didUnmount: unique symbol;
-export const deriveStateFromProps: unique symbol;
+export const propertyChangedCallback: unique symbol;
 export const render: unique symbol;
 export const shouldUpdate: unique symbol;
+export const stateChangedCallback: unique symbol;
 
 export class UnsafeStatic {
   public readonly value: unknown;
+
   public constructor(value: unknown);
 }
 
@@ -34,13 +37,7 @@ export const dhtml: (strings: ReadonlyArray<string>, ...values: unknown[]) => Te
 
 export default class CorpusculeElement extends HTMLElement implements CustomElement {
   public static readonly is: string;
-  public static readonly tag: UnsafeStatic;
-
   public static readonly observableAttributes: ReadonlyArray<PropertyKey>;
-
-  protected static [deriveStateFromProps](props: unknown, states: unknown): unknown | null;
-
-  protected static [shouldUpdate](nextProps: unknown, nextState: unknown, prevProps: unknown, prevState: unknown): boolean;
 
   public readonly elementRendering: Promise<void>;
 
@@ -54,11 +51,11 @@ export default class CorpusculeElement extends HTMLElement implements CustomElem
 
   protected [createRoot](): Element | DocumentFragment;
 
-  protected [didMount](): void;
+  protected [didUpdate](): void;
 
-  protected [didUpdate](prevProperties: unknown, prevStates: unknown): void;
-
-  protected [didUnmount](): void;
+  protected [propertyChangedCallback]<K extends keyof this, V extends this[K]>(propertyName: K, oldValue: V, newValue: V): void;
 
   protected [render](): TemplateResult | null;
+
+  protected [stateChangedCallback](propertyName: keyof this, oldValue: unknown, newValue: unknown): void;
 }
