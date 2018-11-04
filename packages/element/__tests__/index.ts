43a704da24d7d0f4462008d638cafd0378f20ff3
Vlad Rindevich <rindevich.vs@gmail.com>
2018-11-04 14:59:57 +0300

    test(element): remove old tests & add new test for element decorator

--- packages/element/__tests__/index.ts
+++ packages/element/__tests__/index.ts
@@ -1,17 +1,5 @@
-import testCreateComputingEntanglement from './computed';
-import testCorpusculeElement from './corpusculeElement';
-import testDecorators from './decorators';
-import testDhtml from './dhtml';
-import testScheduler from './scheduler';
+import testElementDecorator from './decorators/element';
 
 describe('@corpuscule/element', () => {
-  afterEach(() => {
-    document.body.innerHTML = ''; // tslint:disable-line:no-inner-html
-  });
-
-  testDhtml();
-  testCorpusculeElement();
-  testDecorators();
-  testCreateComputingEntanglement();
-  testScheduler();
+  testElementDecorator();
 });
