628009b344cff9635d6ab006a10cf7c5a2d58d38
Vlad Rindevich <rindevich.vs@gmail.com>
2018-11-05 00:08:52 +0300

    test(element): add new test for state decorator

--- packages/element/__tests__/index.ts
+++ packages/element/__tests__/index.ts
@@ -2,10 +2,12 @@ import testAttributeDecorator from './decorators/attribute';
 import testComputingPair from './decorators/computingPair';
 import testElementDecorator from './decorators/element';
 import testPropertyDecorator from './decorators/property';
+import testStateDecorator from './decorators/state';
 
 describe('@corpuscule/element', () => {
   testAttributeDecorator();
   testComputingPair();
   testElementDecorator();
   testPropertyDecorator();
+  testStateDecorator();
 });
