bf6852724947c6317016b123390168f55db32ec3
Vlad Rindevich <rindevich.vs@gmail.com>
2018-11-04 18:21:27 +0300

    feat(utils): allow "addToRegistry" function to be used for arrays

--- packages/utils/src/addToRegistry.d.ts
+++ packages/utils/src/addToRegistry.d.ts
@@ -1,3 +1,8 @@
-declare const addToRegistry: <T extends object, K, V>(registry: WeakMap<T, Map<K, V>>, target: T, key: K, value: V) => void;
+// tslint:disable:readonly-array
+
+declare const addToRegistry: {
+  <T extends object, K, V>(registry: WeakMap<T, Map<K, V>>, target: T, key: K, value: V): void;
+  <T extends object, V>(registry: WeakMap<T, V[]>, target: T, value: V): void;
+};
 
 export default addToRegistry;
