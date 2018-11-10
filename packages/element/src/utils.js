c4f19f9716fb266e898bf3cbbc75eaadb6b6c71a
Vlad Rindevich <rindevich.vs@gmail.com>
2018-11-10 03:32:31 +0300

    refactor(element): remove unnecessary utils

--- packages/element/src/utils.js
+++ packages/element/src/utils.js
@@ -1,28 +1,3 @@
-export const defaultPropertyOptions = {pure: true};
-
-export const parseAttributeValue = (value, guard) => {
-  switch (guard) {
-    case Boolean:
-      return value !== null;
-    case Number:
-      return Number(value);
-    default:
-      return String(value);
-  }
-};
-
-export const toAttribute = (instance, attributeName, value) => {
-  if (typeof value === 'boolean') {
-    if (value) {
-      instance.setAttribute(attributeName, '');
-    } else {
-      instance.removeAttribute(attributeName);
-    }
-  } else {
-    instance.setAttribute(attributeName, value.toString());
-  }
-};
-
 export const assertElementDecoratorsKindAndPlacement = (decoratorName, kind, placement) => {
   if (kind !== 'field') {
     throw new TypeError(
