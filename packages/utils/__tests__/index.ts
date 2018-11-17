eec178bc51effce261c15f718fd41c96facb9a6f
Vlad Rindevich <rindevich.vs@gmail.com>
2018-11-17 22:54:49 +0300

    feat(utils): add "createDualDescriptor" util

--- packages/utils/__tests__/index.ts
+++ packages/utils/__tests__/index.ts
@@ -1,11 +1,13 @@
 import testAddToRegistry from './addToRegistry';
 import testAsserts from './asserts';
+import testCreateDualDescriptor from './createDualDescriptor';
 import testDescriptors from './descriptors';
 import testGetSuperMethod from './getSuperMethods';
 
 describe('@corpuscule/utils', () => {
   testAddToRegistry();
   testAsserts();
+  testCreateDualDescriptor();
   testDescriptors();
   testGetSuperMethod();
 });
---
+++ packages/utils/src/createDualDescriptor.js
@@ -0,0 +1,26 @@
+import {field} from '../lib/descriptors';
+
+const createDualDescriptor = (descriptor, initializer) => {
+  if (descriptor.get && descriptor.set) {
+    return [descriptor];
+  }
+
+  const storage = Symbol();
+
+  return [
+    {
+      get() {
+        return this[storage];
+      },
+      set(value) {
+        this[storage] = value;
+      },
+    },
+    field({
+      initializer,
+      key: storage,
+    }, {isPrivate: true}),
+  ];
+};
+
+export default createDualDescriptor;
