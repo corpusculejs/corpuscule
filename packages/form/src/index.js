edf41974ef5ef906908615ae3439b1520c88dd62
Vlad Rindevich <rindevich.vs@gmail.com>
2018-11-17 23:28:53 +0300

    feat(form): add Field implementation

--- packages/form/src/index.js
+++ packages/form/src/index.js
@@ -1,4 +1,5 @@
 export {providingValue as formApi} from './context';
+export {default as field} from './field';
 export {default as form} from './form';
 export * from './tokens/field/lifecycle';
 export * from './tokens/form/lifecycle';
