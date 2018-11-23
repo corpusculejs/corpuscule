d0c5c011f94938063b3f25c5e126c8cb544e54eb
Vlad Rindevich <rindevich.vs@gmail.com>
2018-11-24 01:45:06 +0300

    refactor(form): improve implementation & fix several issues
    * Replace accessor objects with functions
    * Use static field initializer for configInitializers instead of finisher
    * disallow partial accessors for formConfig

--- packages/form/src/index.js
+++ packages/form/src/index.js
@@ -1,5 +1,5 @@
 export {providingValue as formApi} from './context';
-export {default as field} from './field';
-export {default as form} from './form';
+export {default as field, fieldConfig} from './field';
+export {default as form, formConfig} from './form';
 export * from './tokens/field/lifecycle';
 export * from './tokens/form/lifecycle';
