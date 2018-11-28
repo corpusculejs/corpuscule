5c73edfe385695e8b6deee47165bbfe71184863f
Vlad Rindevich <rindevich.vs@gmail.com>
2018-11-28 22:18:56 +0300

    refactor: move test mocks to the "test" directory

--- test/mocks/finalForm.ts
+++ test/mocks/finalForm.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import {configOptions, formSubscriptionItems} from 'final-form/dist/final-form.es';
+import {configOptions, formSubscriptionItems} from 'final-form/dist/final-form.es'; // tslint:disable-line:no-implicit-dependencies
 
 export {
   configOptions,
