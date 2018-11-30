ea1f776cc11e81f17afe4161d8e55468c0d29d82
Vlad Rindevich <rindevich.vs@gmail.com>
2018-11-30 18:31:10 +0300

    test: add HTMLElementMock class

--- test/utils.d.ts
+++ test/utils.d.ts
@@ -19,3 +19,28 @@ export class BasicProvider extends HTMLElement {
 
   public connectedCallback(): void;
 }
+
+export class HTMLElementMock {
+  public static readonly observedAttributes: ReadonlyArray<string>;
+  public readonly attributes: ReadonlyMap<string, string>;
+  public readonly listeners: ReadonlyMap<string, ReadonlyArray<(event: Event) => void>>;
+  public readonly nestingChain: ReadonlyArray<HTMLElementMock>;
+  public readonly shadowMock: HTMLDivElement;
+
+  public readonly addEventListener: HTMLElement['addEventListener'];
+  public readonly attachShadow: HTMLElement['attachShadow'];
+  public readonly dispatchEvent: HTMLElement['dispatchEvent'];
+  public readonly getAttribute: HTMLElement['getAttribute'];
+  public readonly hasAttribute: HTMLElement['hasAttribute'];
+  public readonly removeAttribute: HTMLElement['removeAttribute'];
+  public readonly removeEventListener: HTMLElement['removeEventListener'];
+  public readonly setAttribute: HTMLElement['setAttribute'];
+
+  public addParent(parent: HTMLElementMock): void;
+
+  public attributeChangedCallback(key: PropertyKey, oldValue: string | null, newValue: string | null): void;
+
+  public connectedCallback(): void;
+
+  public disconnectedCallback(): void;
+}
