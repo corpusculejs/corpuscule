c117ac996c818f7edf5bf34a7c1227ac7f2ac0c5
Vlad Rindevich <rindevich.vs@gmail.com>
2018-12-02 22:21:37 +0300

    test(router): add tests for createRouter

--- packages/router/__tests__/createRouter.ts
+++ packages/router/__tests__/createRouter.ts
@@ -1,11 +1,61 @@
 import UniversalRouter from 'universal-router';
+import {universalRouterConstructorSpy} from '../../../test/mocks/universal-router';
 import {createRouter} from '../src';
 
 const createRouterTest = () => {
   describe('createRouter', () => {
-    it('should create Universal Router instance', () => {
-      const router = createRouter({});
+    const routes = [
+      {
+        action: () => 'Test',
+      },
+    ];
+    const options = {context: {}};
+
+    it('creates instance of UniversalRouter with custom resolveRoute', () => {
+      const router = createRouter(routes, options);
       expect(router).toEqual(jasmine.any(UniversalRouter));
+      expect(universalRouterConstructorSpy).toHaveBeenCalledWith(routes, {
+        ...options,
+        resolveRoute: jasmine.any(Function),
+      });
+    });
+
+    describe('custom resolveRoute', () => {
+      let resolveRoute: (context: unknown, params: unknown) => [string, unknown];
+
+      beforeEach(() => {
+        createRouter(routes, options);
+
+        [, {resolveRoute}] = universalRouterConstructorSpy.calls.mostRecent().args;
+      });
+
+      it('gets route action result along with context', () => {
+        const actionSpy = jasmine.createSpy('action');
+        actionSpy.and.returnValue('Test');
+
+        const ctx = {
+          route: {
+            action: actionSpy,
+          },
+        };
+
+        const params = {param1: 1};
+
+        expect(resolveRoute(ctx, params)).toEqual(['Test', ctx]);
+        expect(actionSpy).toHaveBeenCalledWith(ctx, params);
+      });
+
+      it('gets undefined if action is not a function', () => {
+        const ctx = {
+          route: {
+            action: null,
+          },
+        };
+
+        const params = {param1: 1};
+
+        expect(resolveRoute(ctx, params)).toBeUndefined();
+      });
     });
   });
 };
