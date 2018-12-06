f4ac3f150bbf37afcf694237bd7e103c19d852aa
Vlad Rindevich <rindevich.vs@gmail.com>
2018-12-07 02:06:39 +0300

    refactor(form): move all lifecycle symbols to single file

--- packages/form/src/index.js
+++ packages/form/src/index.js
@@ -1,5 +1,4 @@
 export {providingValue as formApi} from './context';
 export {default as field, fieldOption} from './field';
 export {default as form, formOption} from './form';
-export * from './tokens/field/lifecycle';
-export * from './tokens/form/lifecycle';
+export * from './tokens/lifecycle';
--- packages/form/src/tokens/form/lifecycle.js
+++
@@ -1,3 +0,0 @@
-export const formState = Symbol('formState');
-export const compareInitialValues = Symbol('compareInitialValues');
-
