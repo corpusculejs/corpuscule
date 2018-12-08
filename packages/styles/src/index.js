ae36483645ba8151728951d0c307394cf27097e5
Vlad Rindevich <rindevich.vs@gmail.com>
2018-12-09 00:24:20 +0300

    refactor(styles): remove lit-html dependency

--- packages/styles/src/index.js
+++ packages/styles/src/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable capitalized-comments */
 import {assertKind} from '@corpuscule/utils/lib/asserts';
 import {field} from '@corpuscule/utils/lib/descriptors';
-import {html} from 'lit-html';
 import {style} from './tokens';
 
 export {link} from './utils';
@@ -12,22 +11,22 @@ const stylePattern = /[{}]/;
 const styles = (...pathsOrStyles) => ({elements, kind}) => {
   assertKind('styles', 'class', kind);
 
+  const template = document.createElement('template');
+  template.innerHTML = pathsOrStyles
+    .map(pathOrStyle =>
+      stylePattern.test(pathOrStyle)
+        ? `<style>${pathOrStyle}</style>`
+        : `<link rel="stylesheet" type="text/css" href="${pathOrStyle}" />`,
+    )
+    .join('');
+
   return {
     elements: [
       ...elements.filter(({key}) => key !== style),
       field(
         {
           initializer() {
-            return html`${
-                pathsOrStyles.map(
-                  (
-                  pathOrStyle, // eslint-disable-line no-extra-parens
-                ) =>
-                  stylePattern.test(pathOrStyle)
-                    ? html`<style>${pathOrStyle}</style>` // prettier-ignore
-                    : html`<link rel="stylesheet" type="text/css" href="${pathOrStyle}" />`, // prettier-ignore
-                )
-              }`; // prettier-ignore
+            return template.content.cloneNode(true);
           },
           key: style,
         },
