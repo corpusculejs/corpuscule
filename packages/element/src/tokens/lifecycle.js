4634c3d5f04a985967913c246aeaa3d88614fb66
Vlad Rindevich <rindevich.vs@gmail.com>
2018-12-18 01:29:36 +0300

    fix(element): remove unnecessary symbols

--- packages/element/src/tokens/lifecycle.js
+++ packages/element/src/tokens/lifecycle.js
@@ -2,6 +2,4 @@ export const createRoot = Symbol('createRoot');
 export const internalChangedCallback = Symbol('internalChangedCallback');
 export const propertyChangedCallback = Symbol('propertyChangedCallback');
 export const render = Symbol('render');
-export const renderer = Symbol('renderer');
-export const scheduler = Symbol('scheduler');
 export const updatedCallback = Symbol('updatedCallback');
