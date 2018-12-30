e197b28e7bc309c63bb489cdb84e11107dda66ef
Vlad Rindevich <rindevich.vs@gmail.com>
2018-12-30 12:03:08 +0300

    test(style): add tests for url cases

--- packages/styles/__tests__/index.ts
+++ packages/styles/__tests__/index.ts
@@ -5,7 +5,71 @@ import styles, {stylesAttachedCallback} from '../src';
 describe('@corpuscule/styles', () => {
   const rawStyles = '.foo{color: red;}';
 
-  it('appends styles to the shadow root', async () => {
+  describe('file links', () => {
+    it('appends "link" tags for urls with same origin', async () => {
+      const [promise, resolve] = createTestingPromise();
+
+      @styles(new URL('./styles.css', location.origin))
+      class Test extends HTMLElement {
+        public constructor() {
+          super();
+          this.attachShadow({mode: 'open'});
+        }
+
+        public connectedCallback(): void {
+          this.shadowRoot!.innerHTML = '<div>Bar</div>';
+        }
+
+        public [stylesAttachedCallback](): void {
+          resolve();
+        }
+      }
+
+      customElements.define(genName(), Test);
+
+      const test = new Test();
+      test.connectedCallback();
+
+      await promise;
+
+      expect(test.shadowRoot!.innerHTML).toBe(
+        `<link rel="stylesheet" type="text/css" href="/styles.css"><div>Bar</div>`,
+      );
+    });
+
+    it('appends "link" tags for urls with different origins', async () => {
+      const [promise, resolve] = createTestingPromise();
+
+      @styles(new URL('./styles.css', 'https://foo'))
+      class Test extends HTMLElement {
+        public constructor() {
+          super();
+          this.attachShadow({mode: 'open'});
+        }
+
+        public connectedCallback(): void {
+          this.shadowRoot!.innerHTML = '<div>Bar</div>';
+        }
+
+        public [stylesAttachedCallback](): void {
+          resolve();
+        }
+      }
+
+      customElements.define(genName(), Test);
+
+      const test = new Test();
+      test.connectedCallback();
+
+      await promise;
+
+      expect(test.shadowRoot!.innerHTML).toBe(
+        `<link rel="stylesheet" type="text/css" href="https://foo/styles.css"><div>Bar</div>`,
+      );
+    });
+  });
+
+  it('appends raw styles to the shadow root', async () => {
     const [promise, resolve] = createTestingPromise();
 
     @styles(rawStyles)
