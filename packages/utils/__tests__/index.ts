f83513ded3f3c67212943d718e05eb9fadd6af08
Vlad Rindevich <rindevich.vs@gmail.com>
2019-01-25 04:53:38 +0300

    refactor(utils): remove createSupers util

--- packages/utils/__tests__/index.ts
+++ packages/utils/__tests__/index.ts
@@ -1,6 +1,5 @@
 import testAddToRegistry from './addToRegistry';
 import testAsserts from './asserts';
-import testCreateSupers from './createSupers';
 import testDescriptors from './descriptors';
 import testPropertyUtils from './propertyUtils';
 import testSchedule from './scheduler';
@@ -9,7 +8,6 @@ describe('@corpuscule/utils', () => {
   testAddToRegistry();
   testAsserts();
   testDescriptors();
-  testCreateSupers();
   testPropertyUtils();
   testSchedule();
 });
