91e573b9da0ba5eb74c0dc594fe5f4f4d6446cc4
Vlad Rindevich <rindevich.vs@gmail.com>
2019-02-01 18:05:09 +0300

    test(utils): add running utils tests

--- packages/utils/__tests__/index.ts
+++ packages/utils/__tests__/index.ts
@@ -1,11 +1,15 @@
 import testAsserts from './asserts';
 import testDescriptors from './descriptors';
+import testGetSupers from './getSupers';
 import testPropertyUtils from './propertyUtils';
 import testSchedule from './scheduler';
+import testShallowEqual from './shallowEqual';
 
 describe('@corpuscule/utils', () => {
   testAsserts();
   testDescriptors();
+  testGetSupers();
   testPropertyUtils();
   testSchedule();
+  testShallowEqual();
 });
