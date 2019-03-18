ad2187945d0deb0fb6334be858c9d946d9230cc4
Vlad Rindevich <rindevich.vs@gmail.com>
2019-03-18 04:16:19 +0300

    build: fix externals setting

--- scripts/project.js
+++ scripts/project.js
@@ -1,21 +1,48 @@
 const packages = {
-  context: ['index'],
-  element: ['index'],
-  form: ['index'],
-  'lit-html-renderer': ['index', 'init', 'shady', 'withCustomElement'],
-  redux: ['index'],
-  router: ['index'],
-  styles: ['index'],
-  utils: [
-    'asserts',
-    'compose',
-    'descriptors',
-    'getSupers',
-    'lifecycleDescriptors',
-    'propertyUtils',
-    'scheduler',
-    'shallowEqual',
-  ],
+  context: {
+    entries: ['index'],
+  },
+  element: {
+    entries: ['index'],
+  },
+  form: {
+    entries: ['index'],
+  },
+  'lit-html-renderer': {
+    entries: ['index', 'init', 'shady', 'withCustomElement'],
+    external: ['init', 'shady'],
+  },
+  redux: {
+    entries: ['index'],
+  },
+  router: {
+    entries: ['index'],
+  },
+  styles: {
+    entries: ['index'],
+  },
+  utils: {
+    entries: [
+      'asserts',
+      'compose',
+      'descriptors',
+      'getSupers',
+      'lifecycleDescriptors',
+      'propertyUtils',
+      'scheduler',
+      'shallowEqual',
+    ],
+    external: [
+      'asserts',
+      'compose',
+      'descriptors',
+      'getSupers',
+      'lifecycleDescriptors',
+      'propertyUtils',
+      'scheduler',
+      'shallowEqual',
+    ],
+  },
 };
 
 const definitions = ['typings'];
