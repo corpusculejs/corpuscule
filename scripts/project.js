d4049da856027d4139f04345309385952de98412
Vlad Rindevich <rindevich.vs@gmail.com>
2019-03-18 03:20:47 +0300

    fix(lit-html-renderer): add init file to the files list

--- scripts/project.js
+++ scripts/project.js
@@ -2,7 +2,7 @@ const packages = {
   context: ['index'],
   element: ['index'],
   form: ['index'],
-  'lit-html-renderer': ['index', 'shady', 'withCustomElement'],
+  'lit-html-renderer': ['index', 'init', 'shady', 'withCustomElement'],
   redux: ['index'],
   router: ['index'],
   styles: ['index'],
