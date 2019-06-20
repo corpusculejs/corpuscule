fef8aea3c6b7aa5745fae4a13eb53368834f32b2
Vlad Rindevich <rindevich.vs@gmail.com>
2019-06-20 23:44:30 +0300

    docs(utils): add an example for the getSupers

--- packages/utils/src/getSupers.ts
+++ packages/utils/src/getSupers.ts
@@ -28,6 +28,39 @@ const noop = () => {};
  *
  * @param fallbacks a list of fallback functions to replace methods which are
  * missing in the `target`.
+ *
+ * @returns an object that has a method for all keys mentioned in the `names`
+ * array. Method could be a target method (both own or inherited), a fallback
+ * function or a noop.
+ *
+ * @example ```typescript
+ * class Foo {
+ *   public foo() {
+ *     console.log('foo called');
+ *   }
+ * }
+ *
+ * class Bar extends Foo {
+ *   public bar() {
+ *     console.log('bar called');
+ *   }
+ * }
+ *
+ * const fallbacks = {
+ *   baz() {
+ *     console.log('baz called');
+ *   }
+ * };
+ *
+ * const bar = new Bar();
+ *
+ * const supers = getSupers(bar, ['foo', 'bar', 'baz', 'boo'], fallbacks);
+ *
+ * supers.foo(); // foo called
+ * supers.bar(); // bar called
+ * supers.baz(); // baz called
+ * supers.boo(); // <nothing happens>
+ * ```
  */
 export default function getSupers<N extends PropertyKey>(
   target: any,
