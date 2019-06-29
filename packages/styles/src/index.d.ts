016fa11d18b1a5bcaec1730193c68a1ac4060677
Vlad Rindevich <rindevich.vs@gmail.com>
2019-06-29 12:31:33 +0300

    docs(styles): move mechanism description to the module top and improve docs

--- packages/styles/src/index.d.ts
+++ packages/styles/src/index.d.ts
@@ -1,12 +1,83 @@
 /**
  * This module provides tools to add styles to a web component.
  *
- * @module @corpuscule/styles
+ * ## How it works
  *
- * @import
+ * There are four ways to add styles to a web component. The [[styles]]
+ * decorator detect the appropriate one automatically and using the
+ * [[stylesAdvanced]] you can define it manually via the
+ * [[StylesDecoratorOptions]].
+ *
+ * ### URL instances
+ * Using an `URL` instance, you can define the path to the CSS file you want to
+ * use for your web component. A `<link>` tag is created in the `ShadowRoot`
+ * and the file is loaded; when the loading is over, the styles are applied to
+ * the component. All styles are scoped, so the result is equal to othe
+ * approaches.
+ *
+ * This approach brings some disadvantages:
+ * * Loading CSS files is a time-consuming process, and while styles are not
+ * loaded, the component might look ugly. To avoid it, you can use
+ * [[stylesAttachedCallback]] method, which is called after all styles are
+ * loaded.
+ * * Loading many small files is quite expensive, and you cannot unite all the
+ * CSS files in a big one: it will break the whole idea of the Shadow DOM.
+ *
+ * Considering all the facts, it may be not a good idea to use this approach in
+ * production; on the other hand, it would be a great addition to the
+ * development process along with the native ES modules because it does not
+ * require any building process.
+ *
+ * ```typescript
+ * import styles, {stylesAttachedCallback} from '@corpuscule/styles';
+ *
+ * @styles(new URL('styles.css', import.meta.url))
+ * class StyledComponent extends HTMLElement {
+ *   public connectedCallback() {
+ *     this.shadowRoot!.innerHTML = '<div hidden class="foo">Bar</div>';
+ *   }
+ *
+ *   private [stylesAttachedCallback]() {
+ *      this.shadowRoot!.querySelector('.foo')!.hidden = false;
+ *   }
+ * }
+ *
+ * customElements.define('styled-component', StyledComponent);
+ * ```
+ *
+ * ### Constructible Stylesheets
+ * The Constructible Stylesheets proposal allows attaching styles directly to a
+ * component `ShadowRoot` as if a browser defined it. No intermediate `<style>`
+ * tag is required.
+ *
+ * ### ShadyCSS
+ * `ShadyCSS` provides support for a Constructible Stylesheets proposal, so
+ * this approach does not differ a lot from the previous one; just the polyfill
+ * is used.
+ *
+ * ### HTMLStyleElement
+ * If no approach described above works, the fallback to a `<style>` tag in the
+ * top of `ShadowRoot` will be used.
+ *
+ * ```typescript
+ * import styles, {stylesAttachedCallback} from '@corpuscule/styles';
+ * import css from './styles.css';
+ *
+ * @styles(css)
+ * class StyledComponent extends HTMLElement {
+ *   public connectedCallback() {
+ *     this.shadowRoot!.innerHTML = '<div class="foo">Bar</div>';
+ *   }
+ * }
+ *
+ * customElements.define('styled-component', StyledComponent);
+ * ```
+ *
  * ```typescript
  * import styles, {stylesAttachedCallback} from '@corpuscule/styles';
  * ```
+ *
+ * @module @corpuscule/styles
  */
 
 /**
@@ -18,26 +89,12 @@
  * fire after the `<style>` tag is mounted.
  * * If nothing above is used, it will fire immediately after the
  * `attachShadow` is called.
- *
- * @example
- * ```typescript
- * @styles(new URL('styles.css', import.meta.url))
- * class Component extends HTMLElement {
- *   public connectedCallback() {
- *     this.shadowRoot!.innerHTML = '<div hidden class="foo">Bar</div>';
- *   }
- *
- *   private [stylesAttachedCallback]() {
- *      this.shadowRoot!.querySelector('.foo')!.hidden = false;
- *   }
- * }
- * ```
  */
 export const stylesAttachedCallback: unique symbol;
 
 export interface StylesDecoratorOptions {
   /**
-   * Defines whether the decorator should use the [Constructable Stylesheet
+   * Defines whether the decorator should use the [Constructible Stylesheet
    * proposal](https://wicg.github.io/construct-stylesheets/).
    */
   readonly adoptedStyleSheets?: boolean;
@@ -48,43 +105,25 @@ export interface StylesDecoratorOptions {
   readonly shadyCSS?: boolean;
 }
 /**
- * A decorator to add styles to a web component. There are four ways to do it;
- * based on the received options and type of the following parameters, the best
- * one will be chosen.
+ * A decorator to add styles to a web component.
  *
- * ## URL instance
- * You can use the `URL` class instance as one of the decorator parameters.
- * This way, the `<link>` tag with the provided URL will be added to the
- * `ShadowRoot` of the component. All styles loaded via the `HTMLLinkElement`
- * are scoped, so this approach would be an excellent addition to the native ES
- * modules while the [CSS modules](https://github.com/w3c/webcomponents/issues/759)
- * proposal is under development.
+ * ### Decision algorithm
+ * This algorithm is applied to each element of the `pathsOrStyles` array.
+ * * The [URL instances](#url-instances) approach is used if the element is an
+ * `URL` instance.
+ * * The [ShadyCSS](#shadycss) approach is used if the element is a string and
+ * the [shadyCSS]{@link StylesDecoratorOptions.shadyCSS}
+ * * The [Constructible Stylesheets](#constructible-stylesheets) approach is
+ * used if the element is a string and browser supports Constructible
+ * Stylesheets proposal.
+ * * The [HTMLStyleElement](#htmlstyleelement) approach will be used if no one
+ * above works.
  *
- * However, loading CSS files is a time-consuming process, and while it is not
- * finished, your component might look ugly without proper styling. To avoid
- * it, you can use the [[stylesAttachedCallback]] method instead of the usual
- * `connectedCallback`. It fires when all styles are loaded.
- *
- * ## Shady CSS
- * `ShadyCSS` has support for Constructable Stylesheets, so using it implies
- * using Constructable Stylesheet polyfill. The algorithm decides to use this
- * method if the path is a string, and the [[shadyCSS]] option is enabled.
- *
- * ## Constructable Stylesheets
- * The [Constructable Stylesheet proposal ](https://wicg.github.io/construct-stylesheets/)
- * allows attaching styles directly to a component via JavaScript without the
- * intermediate `<style>` tag. This way will be chosen if the path is a string,
- * and the [[adoptedStyleSheets]] option is enabled.
- *
- * ## HTMLStyleElement
- * If no other option works, the decorator will add `<style>` tag as a first
- * child of `ShadowRoot`.
+ * @param pathsOrStyles an array with paths to the CSS files (as `URL`
+ * instances) or strings with CSS code.
  *
  * @param options an object that contains options to change the default
  * behavior of the decorator.
- *
- * @param pathsOrStyles an array that can contain strings or `URL` instances
- * that define paths to the desired component styles.
  */
 export function stylesAdvanced(
   pathsOrStyles: Array<string | URL>,
@@ -95,11 +134,11 @@ export function stylesAdvanced(
  * A default version of [[stylesAdvanced]] decorator with automatically
  * detected options from [[StylesDecoratorOptions]].
  *
- * ## Detection algorithm
- * `adoptedStyleSheets` is enabled if the browser supports Constructable
- * Stylesheet proposal.
- *
- * `shadyCSS` is enabled if the ShadyCSS polyfill is used, and its support for
- * the native shadow root is not activated.
+ * ### Detection algorithm
+ * * [adoptedStyleSheets]{@link StylesDecoratorOptions.adoptedStyleSheets} is
+ * enabled if the browser supports Constructible Stylesheet proposal.
+ * * [shadyCSS]{@link StylesDecoratorOptions.shadyCSS} is enabled if the
+ * ShadyCSS polyfill is used, and its support for the native shadow root is not
+ * activated.
  */
 export default function styles(...pathsOrStyles: Array<string | URL>): ClassDecorator;
