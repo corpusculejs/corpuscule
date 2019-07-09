830b934b33d04e89fe41645457407c5c0bf4e20b
Vlad Rindevich <rindevich.vs@gmail.com>
2019-07-09 16:53:27 +0300

    docs(redux): add documentation

--- packages/redux/src/index.d.ts
+++ packages/redux/src/index.d.ts
@@ -1,22 +1,138 @@
-import {Token, TokenCreator} from '@corpuscule/utils/lib/tokenRegistry';
+/**
+ * This module provides a [Redux](https://redux.js.org/) connector for web
+ * components.
+ *
+ * @module @corpuscule/redux
+ */
 
-export {
-  isProvider as isProviderAdvanced,
-  provider as providerAdvanced,
-  value as gearAdvanced,
+/**
+ * Do not remove this comment; it keeps typedoc from misplacing the module
+ * docs.
+ */
+
+import {
+  isProvider as isContextProvider,
+  provider as contextProvider,
+  value as contextValue,
 } from '@corpuscule/context';
+import {Token} from '@corpuscule/utils/lib/tokenRegistry';
 
-export type PropertyGetter<S> = (state: S) => any;
-
+/**
+ * A default version of the [@gearAdvanced]{@link @corpuscule/redux.gearAdvanced}
+ * with the token already provided.
+ */
 export const gear: PropertyDecorator;
+
+/**
+ * A default version of the [@dispatcherAdvanced]{@link @corpuscule/redux.dispatcherAdvanced}
+ * with the token already provided.
+ */
 export const dispatcher: PropertyDecorator;
-export const isProvider: (target: unknown) => boolean;
+
+/**
+ * A default version of the [@isProviderAdvanced]{@link @corpuscule/redux.isProviderAdvanced}
+ * with the token already provided.
+ */
+export function isProvider(target: unknown): boolean;
+
+/**
+ * A default version of the [@providerAdvanced]{@link @corpuscule/redux.providerAdvanced}
+ * with the token already provided.
+ */
 export const provider: ClassDecorator;
+
+/**
+ * A default version of the [@reduxAdvanced]{@link @corpuscule/redux.reduxAdvanced}
+ * with the token already provided.
+ */
 export const redux: ClassDecorator;
-export const unit: <S>(getter: PropertyGetter<S>) => PropertyDecorator;
 
-export const createReduxToken: TokenCreator;
+/**
+ * A default version of the [@unitAdvanced]{@link @corpuscule/redux.unitAdvanced}
+ * with the token already provided.
+ */
+export function unit<S>(getter: (state: S) => any): PropertyDecorator;
 
-export const dispatcherAdvanced: (token: Token) => PropertyDecorator;
-export const reduxAdvanced: (token: Token) => ClassDecorator;
-export const unitAdvanced: <S>(token: Token, getter: PropertyGetter<S>) => PropertyDecorator;
+/**
+ * Creates tokens to bind decorators with each other.
+ */
+export function createReduxToken(): Token;
+
+/**
+ * A decorator that converts a class method or field (with an action creator
+ * assigned) to a redux [dispatcher](https://redux.js.org/api/store#dispatch).
+ * Redux will dispatch everything the wrapped function returns.
+ *
+ * ### Example
+ * ```typescript
+ * const someExternalAction = (value: string) => ({
+ *   type: 'SOME_EXTERNAL_ACTION',
+ *   payload: value,
+ * });
+ *
+ * @redux
+ * class ReduxExample extends HTMLElement {
+ *   @dispatch public fieldExample = someExternalAction;
+ *
+ *   private secretNumber: number = 10;
+ *
+ *   @dispatch
+ *   public methodExample(value: number) {
+ *     return {type: 'SOME_ACTION', payload: value + this.secretNumber};
+ *   }
+ * }
+ * ```
+ *
+ * @param token a token issued by a [[createReduxToken]] function that connects
+ * all decorators in a single working system.
+ */
+export function dispatcherAdvanced(token: Token): PropertyDecorator;
+
+/**
+ * A decorator that works as a [@value]{@link @corpuscule/context.value} for the
+ * [@provider]{@link @corpuscule/redux.providerAdvanced}.
+ */
+export const gearAdvanced: typeof contextValue;
+
+/**
+ * Works as a [isProvider]{@link @corpuscule/context.isProvider} for the
+ * [@provider]{@link @corpuscule/redux.providerAdvanced}.
+ */
+export const isProviderAdvanced: typeof isContextProvider;
+
+/**
+ * A decorator that works as a Redux store provider. See
+ * [@provider]{@link @corpuscule/context.provider} for more information.
+ */
+export const providerAdvanced: typeof contextProvider;
+
+/**
+ * A decorator that makes a class declaration a Redux provider with a store as a
+ * context value. The [@consumer]{@link @corpuscule/context.consumer} decorator
+ * is used internally.
+ *
+ * @param token a token issued by a [[createReduxToken]] function that connects
+ * all decorators in a single working system.
+ */
+export function reduxAdvanced(token: Token): ClassDecorator;
+
+/**
+ * A decorator that makes a class property a reflection for the specific store
+ * value. Whenever the value is changed, the property receives an update as
+ * well.
+ *
+ * ### Example
+ * ```typescript
+ * @redux
+ * class ReduxExample extends HTMLElement {
+ *   @unit(store => store.foo)
+ *   public foo!: number;
+ * }
+ * ```
+ *
+ * @param token a token issued by a [[createReduxToken]] function that connects
+ * all decorators in a single working system.
+ *
+ * @param getter a function that extracts the value to reflect from the store.
+ */
+export function unitAdvanced<S>(token: Token, getter: (state: S) => any): PropertyDecorator;
